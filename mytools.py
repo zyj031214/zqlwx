d23c4e0adcc2e4fc1b5901f73c74082fdc871b5a
zyj031214 <zwc1782cbs@163.com>
2023-11-22 13:55:58 +0800

    自动推送

--- mytools.py
+++ mytools.py
@@ -1,18 +1,26 @@
-import pandas as pd  
-import matplotlib.pyplot as plt  
-plt.rcParams["font.sans-serif"]=["SimHei"]# 设置字体  
-  
+import pandas as pd
+import matplotlib.pyplot as plt
+from pyreadstat import pyreadstat
+plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置字体
+
+
+def 读取SPSS数据文件(文件位置及名称, 是否保留标签值: bool):
+    数据表, metadata = pyreadstat.read_sav(
+        文件位置及名称, apply_value_formats=是否保留标签值, formats_as_ordered_category=True)
+    return 数据表
+
+
 def 有序变量描述统计函数(表名, 变量名):
-    """ 对有序类别变量进行描述统计 """
     result = 表名[变量名].value_counts(sort=False)
     描述统计表 = pd.DataFrame(result)
     描述统计表['比例'] = 描述统计表['count'] / 描述统计表['count'].sum()
     描述统计表['累计比例'] = 描述统计表['比例'].cumsum()
     return 描述统计表
 
-def 绘制直方图(表名):  
-    x=表名.index  
-    y=表名['count'].values  
-    fig,ax2=plt.subplots()  
-    ax2.bar(x,y)  
+
+def 绘制直方图(表名):
+    x = 表名.index
+    y = 表名['count'].values
+    fig, ax2 = plt.subplots()
+    ax2.bar(x, y)
     plt.show()
